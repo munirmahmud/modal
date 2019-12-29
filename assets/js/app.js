d98cf61ab7a3274d48296936f7c178fd9d8b6922
Munir Mahmud <munirmahmud6@gmail.com>
2019-12-30 00:09:38 +0600

    Button event added

---
+++ assets/js/app.js
@@ -0,0 +1,25 @@
+const cardBtn = document.querySelectorAll('.card button');
+const modalWrapper = document.querySelector('.modal-wrapper');
+const modalBody = document.querySelector('.modal-body');
+
+function handleCardButtonClick(e) {
+	const button = e.currentTarget;
+	const card = button.closest('.card');
+	//Grab the image src 
+	const imgSrc = card.querySelector('img').src;
+	const desc = card.dataset.description;
+
+	// Populate the modal with the new info 
+	modalBody.innerHTML = `
+	<img src="${imgSrc.replace('400', '600')}" alt="${desc}">
+	<p class="m-0">${desc}</p>
+	`;
+
+	//Show the modal
+	modalWrapper.classList.add('open');
+}
+
+cardBtn.forEach((button) => button.addEventListener('click', handleCardButtonClick));
+
+
+
